2c70e87f71387f5c2763a177c23a275689228533
Ilya <ilyaskrypnik@gmail.com>
2019-10-20 20:15:17 +0700

    Refactor detectors methods

--- src/main/java/com/ilyaskrypnik/ordersparser/service/FileExtensionDetector.java
+++ src/main/java/com/ilyaskrypnik/ordersparser/service/FileExtensionDetector.java
@@ -4,32 +4,18 @@ import com.ilyaskrypnik.ordersparser.domain.SupportedFileExtensions;
 import com.ilyaskrypnik.ordersparser.exception.UnsupportedExtensionException;
 import org.springframework.stereotype.Service;
 
-import static com.ilyaskrypnik.ordersparser.domain.SupportedFileExtensions.CSV;
-import static com.ilyaskrypnik.ordersparser.domain.SupportedFileExtensions.JSON;
-
 @Service
 public class FileExtensionDetector {
 
     public SupportedFileExtensions getFileExtension(String fileName) throws UnsupportedExtensionException {
-        String fileExtension = fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
+        String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
 
-        if (!isExtensionSupported(fileExtension)) {
-            throw new UnsupportedExtensionException("Unsupported file extension " + fileExtension);
-        }
-
-        if (fileExtension.equals(CSV.name())) {
-            return CSV;
-        } else {
-            return JSON;
-        }
-    }
-
-    private boolean isExtensionSupported(String fileExtension) {
         for (SupportedFileExtensions extension : SupportedFileExtensions.values()) {
             if (fileExtension.equals(extension.name())) {
-                return true;
+                return extension;
             }
         }
-        return false;
+
+        throw new UnsupportedExtensionException("Unsupported file extension " + fileExtension);
     }
 }
